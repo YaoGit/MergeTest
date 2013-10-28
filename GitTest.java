39b3d2fd9ed1806976f135b3ddfdadaf2b877185
markwu <mark24.wu@gmail.com>
2013-10-28 15:13:07 +0800

    add new function

--- GitTest.java
+++ GitTest.java
@@ -5,4 +5,7 @@ public class GitTest {
 	public static void showCommittor(){
 		System.out.println("This is commit by MarkWu24");
 	}
+	public static void addNewUser(){
+		System.out.println("Add a new user to the git");
+	}
 }
