bbe5c4736eb88b9331cce90d77e0de0b801e1dd6
markwu <mark24.wu@gmail.com>
2013-10-28 14:29:24 +0800

    this is commit by markwu

---
+++ GitTest.java
@@ -0,0 +1,8 @@
+public class GitTest {
+	public static void main(String[] args){
+		showCommittor();
+	}
+	public static void showCommittor(){
+		System.out.println("This is commit by MarkWu24");
+	}
+}
